5239e6c7564de78c2aa12dc0f3894ca8b08ceaac
VinayJeurkar <vinay.jeurkar@abmiro.com>
2019-03-30 08:22:13 +0000

    Syntax error

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello World");
-	Systen.out.println("Second Line");
+	System.out.println("Second Line");
     }
 
 }
